68498e8f7bce283820117c00a64992b907fe0026
jeff0brewer <25332698+Jeff0Brewer@users.noreply.github.com>
2023-01-28 14:13:24 -0700

    remove log

--- components/timecard.tsx
+++ components/timecard.tsx
@@ -54,7 +54,6 @@ const Timecard: FC<TimecardProps> = props => {
             if (!entry.clockIn) { pairs.push([entries[i + 1], entries[i]]) }
             return pairs
         }, entryPairs)
-        console.log(entryPairs)
         return entryPairs.map((pair, i) =>
             <DayDisplay in={pair[0]} out={pair[1]} delete={deleteEntries} key={i} />
         )
