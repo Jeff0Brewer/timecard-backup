1a7b535ac4e90b980e27373795d5bcb68d36cc92
jeff0brewer <25332698+Jeff0Brewer@users.noreply.github.com>
2023-04-11 13:09:52 -0600

    add entry response handler

--- lib/fetch-util.ts
+++ lib/fetch-util.ts
@@ -1,3 +1,5 @@
+import { EntryData, EntryResponse } from '@/lib/types'
+
 const postBody = (data: object) => {
     return {
         method: 'POST',
@@ -6,15 +8,27 @@ const postBody = (data: object) => {
     }
 }
 
-const getDateJson = (res: Response) => {
-    return res.json()
-        .then(data => {
-            data.date = new Date(data.date)
-            return data
-        })
+const handleEntryResponse = async (res: Response): Promise<Array<EntryData>> => {
+    const entryResponse: EntryResponse = await res.json()
+    // check and log errors
+    if (!res.ok) {
+        const message = 'message' in entryResponse
+            ? entryResponse.message
+            : 'failed'
+        throw new Error(`Api Error ${res.status}: ${message}`)
+    }
+    // return empty list of entries if success and message
+    if ('message' in entryResponse) {
+        return []
+    }
+    // parse json string dates if entries returned
+    return entryResponse.data.map((entry: EntryData) => {
+        entry.date = new Date(entry.date)
+        return entry
+    })
 }
 
 export {
     postBody,
-    getDateJson
+    handleEntryResponse
 }
